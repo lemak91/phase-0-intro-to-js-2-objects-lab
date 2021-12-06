5645890ae65b59d558f46b7c56d7e317b0e4f03a
John Lemak <lemak91@mac.com>
2021-12-06 08:42:47 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+const employee = {
+    name: "John Lemak",
+    streetAddress: "3651 Cardiff Ave",
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+      
+  }
+
+  function destructivelyEmployeeUpdateWithKeyAndValue(employee, key, value) {
+      
+}
